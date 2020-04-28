1d33880c0aa6fceedb3e56457342b4ee510f5d83
DiveshRKubal <36663489+DiveshRKubal@users.noreply.github.com>
2020-04-28 19:49:19 +0530

    Update new.py

--- new.py
+++ new.py
@@ -219,87 +219,25 @@ def sarcasm(sentence):
     return final_sarcasm
 
 def emotion(sentence):
-        from textblob import Word
-        data = pd.read_csv('text_emotion.csv')
-
-        data = data.drop('author', axis=1)
-
-        # Dropping rows with other emotion labels
-        data = data.drop(data[data.sentiment == 'anger'].index)
-        data = data.drop(data[data.sentiment == 'boredom'].index)
-        data = data.drop(data[data.sentiment == 'enthusiasm'].index)
-        data = data.drop(data[data.sentiment == 'empty'].index)
-        data = data.drop(data[data.sentiment == 'fun'].index)
-        data = data.drop(data[data.sentiment == 'relief'].index)
-        data = data.drop(data[data.sentiment == 'surprise'].index)
-        data = data.drop(data[data.sentiment == 'love'].index)
-        data = data.drop(data[data.sentiment == 'hate'].index)
-        data = data.drop(data[data.sentiment == 'neutral'].index)
-        data = data.drop(data[data.sentiment == 'worry'].index)
-
-        # Making all letters lowercase
-        data['content'] = data['content'].apply(lambda x: " ".join(x.lower() for x in x.split()))
-
-        # Removing Punctuation, Symbols
-        data['content'] = data['content'].str.replace('[^\w\s]',' ')
-
-        # Removing Stop Words using NLTK
-        stop = stopwords.words('english')
-        data['content'] = data['content'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
-
-        #Lemmatisation
-        data['content'] = data['content'].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
-        print(data['content'],"Hello")
-        #Correcting Letter Repetitions
-
-        def de_repeat(text):
-            pattern = re.compile(r"(.)\1{2,}")
-            return pattern.sub(r"\1\1", text)
-
-        data['content'] = data['content'].apply(lambda x: " ".join(de_repeat(x) for x in x.split()))
-
-        # Code to find the top 10,000 rarest words appearing in the data
-        freq = pd.Series(' '.join(data['content']).split()).value_counts()[-10000:]
-
-        # Removing all those rarely appearing words from the data
-        freq = list(freq.index)
-        data['content'] = data['content'].apply(lambda x: " ".join(x for x in x.split() if x not in freq))
-
-        #Encoding output labels 'sadness' as '1' & 'happiness' as '0'
-        lbl_enc = preprocessing.LabelEncoder()
-        y = lbl_enc.fit_transform(data.sentiment.values)
-
-        # Splitting into training and testing data in 90:10 ratio
-        X_train, X_val, y_train, y_val = train_test_split(data.content.values, y, stratify=y, random_state=42, test_size=0.1, shuffle=True)
-
-        # Extracting TF-IDF parameters
-        tfidf = TfidfVectorizer(max_features=1000, analyzer='word',ngram_range=(1,3))
-        X_train_tfidf = tfidf.fit_transform(X_train)
-        X_val_tfidf = tfidf.fit_transform(X_val)
-
-        # Extracting Count Vectors Parameters
-        count_vect = CountVectorizer(analyzer='word')
-        count_vect.fit(data['content'])
-        X_train_count =  count_vect.transform(X_train)
-        X_val_count =  count_vect.transform(X_val)
-
-
-        lsvm = SGDClassifier(alpha=0.001, random_state=5, max_iter=15, tol=None)
-        lsvm.fit(X_train_count, y_train)
-
         tweets = pd.DataFrame([sentence])
 
         # Doing some preprocessing on these tweets as done before
         tweets[0] = tweets[0].str.replace('[^\w\s]',' ')
+        from nltk.corpus import stopwords
         stop = stopwords.words('english')
         tweets[0] = tweets[0].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
         from textblob import Word
         tweets[0] = tweets[0].apply(lambda x: " ".join([Word(word).lemmatize() for word in x.split()]))
+        import pickle
 
+        pickle_in = open("count_vec.pkl","rb")
+        count_vect = pickle.load(pickle_in)
         tweet_count = count_vect.transform(tweets[0])
-
-        tweet_pred = lsvm.predict(tweet_count)
-        if tweet_pred ==0:
+        
+        # load the model from disk
+        loaded_model = pickle.load(open('lsvm_model.pkl', 'rb'))
+        result = loaded_model.predict(tweet_count)
+        if result  == 0:
             my_ans = "Happy Comment"
             
         else:
