22fdf1fb8c6aab44c9884589e8fc17cf33f0bba5
DiveshRKubal <36663489+DiveshRKubal@users.noreply.github.com>
2020-04-30 00:17:08 +0530

    Update new.py

--- new.py
+++ new.py
@@ -194,12 +194,12 @@ def sarcasm(sentence):
 def face(sentence):
     
     #load json and create model
-    json_file = open('/content/drive/My Drive/Colab Notebooks/model_4layer_2_2_pool.json', 'r')
+    json_file = open('model_4layer_2_2_pool.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     model = model_from_json(loaded_model_json)
     #load weights from h5 file
-    model.load_weights("/content/drive/My Drive/Colab Notebooks/model_4layer_2_2_pool.h5")
+    model.load_weights("model_4layer_2_2_pool.h5")
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=[categorical_accuracy])
     
     img = image.load_img(sentence, target_size=(48, 48), grayscale=True)
